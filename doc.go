75049aeb5bfcf0b3b88d16f9b9cfccbabbad99cf
Patrice FERLET <metal3d@gmail.com>
2015-11-01 16:24:23 +0100

    Documentation

--- doc.go
+++ doc.go
@@ -6,6 +6,41 @@ Kwiscale can also serve Websockets.
 
 Kwiscale provides a basic template system based on http/template from Go SDK and has got a plugin system to provides other template engines.
 
+
+The built-in template allows to override templates. Example:
+
+	// templates/main.go
+	<body>
+	{{ template "CONTENT" .}}
+	</body>
+
+	// template/home/index.go (remove the "\" in override directive)
+	{{/* override "main.go" *\/}}
+
+	{{ define "CONTENT" }}
+	<p>Hello workd</p>
+	{{ end }}
+
+
+Also, built-in template provides two functions:
+
+	- url: gives url of a handler with parameters
+	- static: gives static resource url
+
+Example:
+
+	<link rel="stylesheet" href="{{static "/css/styles.css"}}" />
+	<a href="{{ url "home" }}">Home</a>
+	<a href="{{ url "user" "id" 12345  }}">User</a>
+	<a href="{{ url "handlers.UserHandler" "id" 12345  }}">User</a>
+
+For "url" function, the first argument can be:
+
+	- alias for handler
+	- handler name
+
+Others arguments are the pair "key value".
+
 See http://gopkg.in/kwiscale/template-pongo2.v1 to use Pongo2.
 
 To handle HTTP Verbs:
@@ -24,11 +59,26 @@ To handle HTTP Verbs:
 	}
 
 
+To be able to use configuration file (yaml), you MUST register handlers. The common way to do is to use "init()" function in you handlers package:
+
+	package handlers
+
+	import "gopkg.in/kwiscale/framework.v1"
+
+	func init(){
+		kwiscale.Register(&HomeHandler{})
+	}
+
+	type HomeHandler struct {kwiscale.HomeHandler}
+	//...
+
+Note: if you're using kwiscale CLI, `kwiscale new handler` command create the register call for you.
+
 Kwiscale provide a way to have method with parameter that are mapped from the given route.
 
 Example:
 
-	app.AddRoute(`/user/{name:.+/}{id:\d+}/`)
+	app.AddRoute(`/user/{name:.+/}{id:\d+}/`, UserHandler{})
 
 	//...
 	type UserHandler struct {kwiscale.RequestHandler}
@@ -44,6 +94,9 @@ Note that parameters names for Get() method are not used to map url values. Kwis
 	}
 
 Anyway, you always may use "UserHandler.Vars":  UserHandler.Vars["id"] and UserHandler.Vars["name"] that are `string` typed.
+
+
+You may use Init() and Destroy() method that are called before and after HTTP verb invocation. You may, for example, open database connection in "Init" and close the connection in "Destroy".
 
 
 Kwiscale provides a CLI:
