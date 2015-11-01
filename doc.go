d4fde54a79a19eaecb315263f220ab65144e998a
Patrice FERLET <metal3d@gmail.com>
2015-11-01 16:08:53 +0100

    Addapt documentation

--- doc.go
+++ doc.go
@@ -4,14 +4,13 @@ Handlers that handles HTTP Verbs methods. Kwiscale can handle basic HTTP Verbs a
 
 Kwiscale can also serve Websockets.
 
-It provides a basic template system based on http/template from Go SDK and has got a plugin system to provides other template
-engines
+Kwiscale provides a basic template system based on http/template from Go SDK and has got a plugin system to provides other template engines.
 
 See http://gopkg.in/kwiscale/template-pongo2.v1 to use Pongo2.
 
 To handle HTTP Verbs:
 
-	type HomeHandler struct {kwiscale.RequetHandler}
+	type HomeHandler struct {kwiscale.RequestHandler}
 	func (home *HomeHandler) Get(){
 		// This will respond to GET call
 		home.WriteString("Hello !")
@@ -23,6 +22,57 @@ To handle HTTP Verbs:
 		// Default listening on 8000 port
 		app.ListenAndServe()
 	}
+
+
+Kwiscale provide a way to have method with parameter that are mapped from the given route.
+
+Example:
+
+	app.AddRoute(`/user/{name:.+/}{id:\d+}/`)
+
+	//...
+	type UserHandler struct {kwiscale.RequestHandler}
+	func (handler *UserHandler) Get(name string, id int) {
+		// ...
+	}
+
+Note that parameters names for Get() method are not used to map url values. Kwiscale maps values repecting the order found in the route. So you may declare Get() method like this:
+
+
+	func (handler *UserHandler) Get(a string, b int) {
+		// ...
+	}
+
+Anyway, you always may use "UserHandler.Vars":  UserHandler.Vars["id"] and UserHandler.Vars["name"] that are `string` typed.
+
+
+Kwiscale provides a CLI:
+
+	go get gopkg.in/framework/kwiscale
+	kwiscale --help
+
+	NAME:
+	   kwiscale - tool to manage kwiscale application
+
+	USAGE:
+	   kwiscale [global options] command [command options] [arguments...]
+
+	VERSION:
+	   0.0.1
+
+	COMMANDS:
+	   new		Generate resources (application, handlers...)
+	   generate	Parse configuration and generate handlers, main file...
+	   help, h	Shows a list of commands or help for one command
+
+	GLOBAL OPTIONS:
+	   --project "kwiscale-app"	project name, will set $GOPATH/src/[projectname] [$KWISCALE_PROJECT]
+	   --handlers "handlers"	handlers package name [$KWISCALE_HANDLERS]
+	   --help, -h			show help
+	   --generate-bash-completion
+	   --version, -v		print the version
+
+See http://readthedocs.org/projects/kwiscale/
 
 */
 package kwiscale
